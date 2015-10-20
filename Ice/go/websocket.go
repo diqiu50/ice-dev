4793f96450fdfc31b19baddefa86e5b4bba06640
ice <ice@jimodb.com>
2015-10-20 21:17:02 +0800

    modify websocket.go

--- Ice/go/websocket.go
+++ Ice/go/websocket.go
@@ -33,7 +33,7 @@ func echo(ws *websocket.Conn) {
 }
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.Handle("/", http.FileServer(http.Dir("webpage")))
 	http.Handle("/echo", websocket.Handler(echo))
 	err := http.ListenAndServe("192.168.99.184:3700", nil)
 	if err != nil {
@@ -42,20 +42,22 @@ func main() {
 }
 
 func getSysinfo() string{
-	var io_bytes int64 = 0;
+	var io_r_bytes int64 = 0;
+	var io_w_bytes int64 = 0;
 	rlst3, _ := disk.DiskIOCounters()
 	for k, v := range rlst3 {
 		if len(k) == 3 {
-			io_bytes += int64(v.ReadBytes)
-			io_bytes += int64(v.WriteBytes)
+			io_r_bytes += int64(v.ReadBytes)
+			io_w_bytes += int64(v.WriteBytes)
 		}
 
 	}
 
-	var net_io_bytes int64 = 0;
+	var net_io_s_bytes int64 = 0;
+	var net_io_r_bytes int64 = 0;
 	rlst4, _ := net.NetIOCounters(false)
-	net_io_bytes += int64(rlst4[0].BytesSent);
-	net_io_bytes += int64(rlst4[0].BytesRecv);
+	net_io_s_bytes += int64(rlst4[0].BytesSent);
+	net_io_r_bytes += int64(rlst4[0].BytesRecv);
 
 	rslt1, _ := cpu.CPUPercent(1*time.Second, false);
 	cpu := strconv.FormatFloat(rslt1[0], 'f', 2, 64);
@@ -66,22 +68,26 @@ func getSysinfo() string{
 	rlst3, _ = disk.DiskIOCounters()
 	for k, v := range rlst3 {
 		if len(k) == 3 {
-			io_bytes -= int64(v.ReadBytes)
-			io_bytes -= int64(v.WriteBytes)
+			io_r_bytes -= int64(v.ReadBytes)
+			io_w_bytes -= int64(v.WriteBytes)
 		}
 	}
-	diskio := strconv.FormatFloat(math.Abs(float64(io_bytes))/(90*1024*1024/100), 'f', 2, 64);
+	diskio_r := strconv.FormatFloat(math.Abs(float64(io_r_bytes))/(90*1024*1024/100), 'f', 2, 64);
+	diskio_w := strconv.FormatFloat(math.Abs(float64(io_w_bytes))/(90*1024*1024/100), 'f', 2, 64);
 
 	rlst4, _ = net.NetIOCounters(false)
-	net_io_bytes -= int64(rlst4[0].BytesSent);
-	net_io_bytes -= int64(rlst4[0].BytesRecv);
-	netio := strconv.FormatFloat(math.Abs(float64(net_io_bytes))/(90*1024*1024/100), 'f', 2, 64);
+	net_io_s_bytes -= int64(rlst4[0].BytesSent);
+	net_io_r_bytes -= int64(rlst4[0].BytesRecv);
+	netio_s := strconv.FormatFloat(math.Abs(float64(net_io_s_bytes))/(90*1024*1024/100), 'f', 2, 64);
+	netio_r := strconv.FormatFloat(math.Abs(float64(net_io_r_bytes))/(90*1024*1024/100), 'f', 2, 64);
 
 	str := "{"
 	str += "\"cpu_usage\":\"" + cpu + "\","
 	str += "\"mem_usage\":\"" + mem + "\","
-	str += "\"disk_usage\":\"" + diskio + "\","
-	str += "\"net_usage\":\"" + netio + "\""
+	str += "\"disk_usage_read\":\"" + diskio_r + "\","
+	str += "\"disk_usage_write\":\"" + diskio_w + "\","
+	str += "\"net_usage_send\":\"" + netio_s + "\","
+	str += "\"net_usage_recv\":\"" + netio_r + "\""
 	str += "}"
 	return str
 }
